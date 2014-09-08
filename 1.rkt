6c581accdd68222ed15229a9d767c286c1050ed8
berhanu <berhanusg@gmail.com>
2014-09-07 20:53:42 -0700

    Worked on Ex 1 to Ex 5
    developed contract statements, purpose etc.. tried function

--- 1.rkt
+++ 1.rkt
@@ -2,6 +2,13 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 
+
+
 ; Question 1 the evaluation could not be saved !! lost my work
+;; Contract:Seconds in leap year; number number number -> number
+;; Purpose : to cmpute the number of seconds in a lip year
+;; Example : ( Number of seconds  366 24 60 60) should yield 31622400 seconds
+;; Definition : 
+( define ( Number of seconds days Hours in a day  Minutes in one hour  Seconds in one hour)
 ;(* (* 366 24)(* 60 60))
 ;31622400
--- 2.rkt
+++ 2.rkt
@@ -1,6 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |2|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; Contract: Tests for inequality: Number Number -> Boolean
+;; Purpose Checks for operand first is greater than operand second and returns true or else false if not
+;; ( 100/ 3) > ((100 +3)* ( 3 * 3))=> True
+(define check for inequaity  100/3 > 100 +3 * 3 *3)
+
 ; > (> ( / 100 3 ) (/ (+ 100 3 ) ( + 3 3)))
 ;true
 ;> 
